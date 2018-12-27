1cb82c04df91ec5a46617c2b474f1118261281dd
Justin Dowell <JulesLetters@gmail.com>
2018-12-26 22:57:09 -0600

    Improve rolling card statistic performance.
    Dramatically speed up the process by making it more
    math-based instead of using a brute-force solution.

--- rolling_card_statistics.py
+++ rolling_card_statistics.py
@@ -1,8 +1,10 @@
-import itertools
+import operator
 from collections import Counter
+from functools import reduce
 
 from multiset import FrozenMultiset
 
+import rolling_draw_types
 from simple_timer_context import SimpleTimerContext
 
 
@@ -14,10 +16,26 @@ def generate(deck: FrozenMultiset) -> Counter:
 
     with SimpleTimerContext("Generating all rolling card arrangements."):
         result = Counter()
-        for i in range(0, len(rolling_cards) + 1):
-            print(i)
-            rolling_permutations_of_i_length = itertools.permutations(rolling_cards, i)
-            for p in rolling_permutations_of_i_length:
-                result.update({FrozenMultiset(p)})
+        rolling_cards_multiset = FrozenMultiset(rolling_cards)
+        draw_types = rolling_draw_types.generate_from(rolling_cards_multiset)
+
+        for draw_type in draw_types:
+            arrangements = factorial(len(draw_type))
+            card_types = draw_type.distinct_elements()
+            for card_type in card_types:
+                arrangements *= ncr(rolling_cards_multiset[card_type], draw_type[card_type])
+
+            result.update({draw_type: arrangements})
 
     return result
+
+
+def ncr(n: int, r: int) -> int:
+    r = min(r, n - r)
+    numerator = reduce(operator.mul, range(n, n - r, -1), 1)
+    denominator = reduce(operator.mul, range(1, r + 1), 1)
+    return numerator // denominator
+
+
+def factorial(n: int) -> int:
+    return reduce(operator.mul, range(n, 0, -1), 1)
