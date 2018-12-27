afd84498e13173f20a45e798f25e09cd18554ee4
Justin Dowell <JulesLetters@gmail.com>
2018-12-26 19:57:37 -0600

    Add single-deck generation to deck_generator.
    I noticed I was doing this a lot in testing,
    so I made it a real function.
    
    Also added better Typing.

--- deck_generator.py
+++ deck_generator.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import List, Callable
+from typing import List, Callable, Set
 
 from multiset import FrozenMultiset
 
@@ -9,20 +9,24 @@ _default_deck = ['+0', '+0', '+0', '+0', '+0', '+0',
                  '-2', '+2', 'x0', 'x2']
 
 
-def get_default_deck():
+def get_default_deck() -> FrozenMultiset:
     return FrozenMultiset(_default_deck)
 
 
-def generate_all_decks_for(perks: List[Callable[[List[str]], None]]):
+def generate_all_decks_for(perks: List[Callable[[List[str]], None]]) -> Set[FrozenMultiset]:
     result = set()
     perk_combinations = []
     for i in range(0, len(perks) + 1):
         perk_combinations += itertools.combinations(perks, i)
 
     for perk_combo in perk_combinations:
-        deck = _default_deck.copy()
-        for func in perk_combo:
-            func(deck)
-        result.add(FrozenMultiset(deck))
+        result.add(generate_deck(perk_combo))
 
     return result
+
+
+def generate_deck(perks: List[Callable[[List[str]], None]]) -> FrozenMultiset:
+    deck = _default_deck.copy()
+    for func in perks:
+        func(deck)
+    return FrozenMultiset(deck)
