4d211ddec62befd7996291a9d6c44c817257b129
Justin Dowell <JulesLetters@gmail.com>
2020-09-02 08:46:06 -0500

    Refactor DrawSchemeStastistics to a class file

---
+++ deck_analyzer/deck_scheme_statistics.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+from fractions import Fraction
+
+from deck_analyzer.aggregated_line import AggregatedLine
+
+
+class DrawSchemeStatistics:
+    def __init__(self) -> None:
+        self.countable_effect_odds = defaultdict(Fraction)
+        self.singular_effect_odds = defaultdict(Fraction)
+        self.total_odds = Fraction()
+        self.expected_damage = Fraction()
+        self.attack_calculations = defaultdict(Fraction)
+
+    def add_aggregated_line(self, aggregate: AggregatedLine, odds: Fraction) -> None:
+        for effect, count in aggregate.countable_effects.items():
+            self.countable_effect_odds[(effect, count)] += odds
+            self.singular_effect_odds[effect] += odds  # Chance a countable occurred.
+        for effect in aggregate.singular_effects:
+            self.singular_effect_odds[effect] += odds
+        self.total_odds += odds
+
+        aggregate_key = tuple(aggregate.atk_calculation)
+        self.attack_calculations[aggregate_key] += odds
+
+    def calculate_expected_damage(self, atk) -> None:
+        for atk_calc, odds in self.attack_calculations.items():
+            damage_from_card_sequence = atk
+            for card_bonus in atk_calc:
+                damage_from_card_sequence = card_bonus.calculation(damage_from_card_sequence)
+            self.expected_damage += damage_from_card_sequence * odds
+
+    def make_copy(self):
+        copy = DrawSchemeStatistics()
+        copy.countable_effect_odds = self.countable_effect_odds.copy()
+        copy.singular_effect_odds = self.singular_effect_odds.copy()
+        copy.total_odds = self.total_odds
+        copy.expected_damage = self.expected_damage
+        copy.attack_calculations = self.attack_calculations.copy()
+        return copy
--- deck_analyzer/main.py
+++ deck_analyzer/main.py
@@ -10,6 +10,7 @@ from multiset import FrozenMultiset
 
 from deck_analyzer import perk_sheets, deck_generator
 from deck_analyzer.aggregated_line import AggregatedLine
+from deck_analyzer.deck_scheme_statistics import DrawSchemeStatistics
 from deck_analyzer.simple_timer_context import SimpleTimerContext
 
 ATK_RANGE = range(0, 4)
@@ -17,42 +18,6 @@ EITHER_ORDER = 2
 AggregatedLineWithOdds = namedtuple("AggregatedLineWithOdds", "aggregated_line odds")
 DeckStatistics = namedtuple("DeckStatistics", "normal advantage")
 Analysis = namedtuple("Analysis", "statistics_by_atk generation_methods")
-
-
-class DrawSchemeStatistics:
-    def __init__(self) -> None:
-        self.countable_effect_odds = defaultdict(Fraction)
-        self.singular_effect_odds = defaultdict(Fraction)
-        self.total_odds = Fraction()
-        self.expected_damage = Fraction()
-        self.attack_calculations = defaultdict(Fraction)
-
-    def add_aggregated_line(self, aggregate: AggregatedLine, odds: Fraction) -> None:
-        for effect, count in aggregate.countable_effects.items():
-            self.countable_effect_odds[(effect, count)] += odds
-            self.singular_effect_odds[effect] += odds  # Chance a countable occurred.
-        for effect in aggregate.singular_effects:
-            self.singular_effect_odds[effect] += odds
-        self.total_odds += odds
-
-        aggregate_key = tuple(aggregate.atk_calculation)
-        self.attack_calculations[aggregate_key] += odds
-
-    def calculate_expected_damage(self, atk):
-        for atk_calc, odds in self.attack_calculations.items():
-            damage_from_card_sequence = atk
-            for card_bonus in atk_calc:
-                damage_from_card_sequence = card_bonus.calculation(damage_from_card_sequence)
-            self.expected_damage += damage_from_card_sequence * odds
-
-    def make_copy(self):
-        copy = DrawSchemeStatistics()
-        copy.countable_effect_odds = self.countable_effect_odds.copy()
-        copy.singular_effect_odds = self.singular_effect_odds.copy()
-        copy.total_odds = self.total_odds
-        copy.expected_damage = self.expected_damage
-        copy.attack_calculations = self.attack_calculations.copy()
-        return copy
 
 
 def derive_statistics(deck, atk_range) -> Dict[int, DeckStatistics]:
